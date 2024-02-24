32fb9ad066901206a0ae71411f1eb081a86d3234
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2024-02-24 13:03:42 +0100

    Add button border

--- resources/views/components/event-alert.blade.php
+++ resources/views/components/event-alert.blade.php
@@ -2,5 +2,5 @@
     <p class="!text-2xl text-background mb-3">Komm an die Chaot*innen-Party</p>
     <h2 class="text-background !mt-0 !mb-6">Sei ein*e Chaot*in!</h2>
     <p><b class="!text-background">Save the date:</b> Samstag, 02.03. 23:00 Uhr im UMBO (Wasserwerkstrasse 89a, 8037 Zürich)</p>
-    <a href="https://www.umbo.wtf/event/chaot-innen-party-soli-rave" class="block p-4 bg-background text-accent hover:text-background hover:bg-accent transition-all text-2xl text-center font-bold mt-8" target="_blank">Mehr Infos</a>
+    <a href="https://www.umbo.wtf/event/chaot-innen-party-soli-rave" class="block p-4 bg-background text-accent hover:text-background hover:bg-accent border-solid border-2 border-background transition-all text-2xl text-center font-bold mt-8" target="_blank">Mehr Infos</a>
 </div>
